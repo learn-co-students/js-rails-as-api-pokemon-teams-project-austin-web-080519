0b889ccace358192aa09879ce21458310bb22d07
Jason <jas0nle@yahoo.com>
2019-09-20 18:06:16 -0500

    almost done

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,26 @@
 class PokemonsController < ApplicationController
+
+    def index
+        pokemons = Pokemon.all
+        render json: pokemons, include: [:trainer]
+    end
+
+    def show
+        pokemon = Pokemon.find_by(id: params[:id])
+        render json: pokemon, include: [:trainer]
+    end
+
+    def create
+        name = Faker::Name.first_name
+        species = Faker::Games::Pokemon.name
+        pokemon = Pokemon.create(nickname: name, species: species, trainer_id: params[:trainer_id])
+        render json: pokemon
+    end
+
+    def destroy
+        pokemon = Pokemon.find_by(id: params[:id])
+        pokemon.destroy
+        render json: pokemon
+    end
+
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -3,12 +3,79 @@ const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 const main = document.querySelector("main")
 
+function addPokemon(trainer) {
+    let pokemonData = {
+        trainer_id: trainer.id
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(pokemonData)
+    };
+
+    console.log(configObj);
+    fetch(POKEMONS_URL, configObj)
+    .then( response => {
+        return response.json()
+    })
+    .then( pokeData => {
+        console.log(pokeData);
+        const li = document.createElement("li");
+        const ul = document.querySelector(`[trainer-list-id="${trainer.id}"]`)
+        li.innerHTML = `${pokeData.nickname} (${pokeData.species})`
+        li.setAttribute("pokemon-list-id", `${pokeData.id}`);
+        const deleteButton = document.createElement("button");
+        deleteButton.setAttribute("class", "release");
+        deleteButton.setAttribute("data-pokemon-id", `${pokeData.id}`);
+        deleteButton.innerText = "Release";
+        createBtnEventListener(deleteButton, pokeData);
+        li.appendChild(deleteButton);
+        ul.appendChild(li);
+    })
+}
+
+function releasePokemon(pokemon) {
+
+    let pokemonData = {
+        id: pokemon.id,
+        nickname: pokemon.nickname,
+        species: pokemon.species,
+        trainer_id: pokemon.trainer_id
+    }
+
+    let configObj = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pokemonData)
+    }
+
+    fetch(`${POKEMONS_URL}/${pokemon.id}`, configObj)
+    .then( response => response.json())
+    .then( pokeData => {
+        console.log(pokeData);
+        const pokeItem = document.querySelector(`[pokemon-list-id="${pokeData.id}"]`)
+        console.log(pokeItem);
+        pokeItem.parentNode.removeChild(pokeItem);
+    })
+}
+
+function createBtnEventListener(button, pokemon) {
+    button.addEventListener("click", event => {
+        releasePokemon(pokemon);
+    })
+}
+
 document.addEventListener("DOMContentLoaded", event => {
+
 
     fetch(TRAINERS_URL)
     .then( response => response.json())
     .then( trainerData => {
-        console.log(trainerData);
         for (const trainer of trainerData) {
             const div = document.createElement("div");
             const para = document.createElement("p");
@@ -16,23 +83,43 @@ document.addEventListener("DOMContentLoaded", event => {
             div.appendChild(para)
             div.setAttribute("class", "card");
             div.setAttribute("data-id", `${trainer.id}`);
-            const addPokemon = document.createElement("button");
-            addPokemon.setAttribute("data-trainer-id", `${trainer.id}`);
-            addPokemon.innerText = "Add Pokemon";
-            div.appendChild(addPokemon);
+            const addPokemonBtn = document.createElement("button");
+            addPokemonBtn.setAttribute("data-trainer-id", `${trainer.id}`);
+            addPokemonBtn.setAttribute("class", "add")
+            
+            addPokemonBtn.innerText = "Add Pokemon";
+            
+            addPokemonBtn.addEventListener("click", event => {
+                
+                if (trainer.pokemons.length >= 6){
+                    return;
+                } else {
+                    addPokemon(trainer);
+                }
+            })
+            
+            div.appendChild(addPokemonBtn);
+            
             const ul = document.createElement("ul");
+            ul.setAttribute("trainer-list-id", `${trainer.id}`);
             div.appendChild(ul);
+            
             const pokemons = trainer.pokemons
             for (const pokemon of pokemons) {
                 const li = document.createElement("li");
-                li.innerHTML = `${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button>`
+                li.setAttribute("pokemon-list-id", `${pokemon.id}`)
+                li.innerHTML = `${pokemon.nickname} (${pokemon.species})`;
+                const deleteButton = document.createElement("button");
+                deleteButton.setAttribute("class", "release");
+                deleteButton.setAttribute("data-pokemon-id", `${pokemon.id}`);
+                deleteButton.innerText = "Release";
+                createBtnEventListener(deleteButton, pokemon);
+                li.appendChild(deleteButton);
                 ul.appendChild(li);
             }
             main.appendChild(div);
         }
     })
-
-
 
 
 })
